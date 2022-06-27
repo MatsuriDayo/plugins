c92b05a558d0860037a859148a3e6e63bbdc0b1a
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2022-06-27 11:54:36 +0800

    xray v1.5.8

--- app_xray/build.gradle.kts
+++ app_xray/build.gradle.kts
@@ -8,7 +8,7 @@ setupAll()
 android {
     defaultConfig {
         applicationId = "moe.matsuri.plugin.xray"
-        versionCode = 5
-        versionName = "v1.5.7-2"
+        versionCode = 6
+        versionName = "v1.5.8-2"
     }
 }
