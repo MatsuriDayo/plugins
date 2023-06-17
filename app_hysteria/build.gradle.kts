e5cb1715cf9d6ff3e7edff3b90c95951bad16e42
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2023-06-17 12:29:38 +0900

    hysteria 1.3.5

--- app_hysteria/build.gradle.kts
+++ app_hysteria/build.gradle.kts
@@ -8,7 +8,7 @@ setupAll()
 android {
     defaultConfig {
         applicationId = "moe.matsuri.exe.hysteria"
-        versionCode = 7
-        versionName = "1.3.4-1"
+        versionCode = 8
+        versionName = "1.3.5-1"
     }
 }
